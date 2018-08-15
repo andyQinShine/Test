10aaf4e9cea581458ae436e3d1442637502f1ac6
andyQinShine <xiaojun2505@gmail.com>
2018-08-15 21:17:27 +0800

    init commit

---
+++ app/web/Keys.py
@@ -0,0 +1,11 @@
+# 此处填写APIKEY
+
+ACCESS_KEY = "aef1401d-d343ab37-233c3fcd-d0846"
+SECRET_KEY = "e71364db-f14a335a-d667d6b0-34025"
+
+# API 请求地址
+MARKET_URL = "https://api.huobi.pro" # https://www.huobipro.com
+TRADE_URL = "https://api.huobi.pro"
+
+# 首次运行可通过get_accounts()获取acct_id,然后直接赋值,减少重复获取。
+ACCOUNT_ID = 278090
---
+++ test/web/test_houbiService.py
@@ -0,0 +1,23 @@
+import unittest
+from app.web import HuobiServices as houbi
+
+class TestHoubiService(unittest.TestCase):
+    symbol = "eosbtc"
+    period = "5min"
+
+    def test_get_kinLine(self):
+        data = houbi.get_kline(self.symbol,self.period)
+        print(data)
+        self.assertIsNotNone(data)
+        self.assertEqual(data['status'], 'ok')
+
+    def test_get_account(self):
+        data = houbi.get_accounts()
+        print(data)
+        self.assertIsNotNone(data)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
